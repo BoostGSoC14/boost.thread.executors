612508dcdec6e88977cf43369fd7f3d79fdbc07c
Ian Forbes <ian.forbes@mail.mcgill.ca>
2014-05-25 16:05:44 -0400

    Added sync_timed_queue with declarations onlys.

---
+++ include/boost/thread/sync_timed_queue.hpp
@@ -0,0 +1,52 @@
+#ifndef BOOST_THREAD_SYNC_TIMED_QUEUE_HPP
+#define BOOST_THREAD_SYNC_TIMED_QUEUE_HPP
+
+#include <boost/chrono/time_point.hpp>
+#include <boost/thread/sync_priority_queue.hpp>
+#include <boost/thread/executors/work.hpp>
+
+#include <boost/config/abi_prefix.hpp>
+
+namespace boost
+{
+
+  template<typename T>
+  struct scheduled_type
+  {
+    T data;
+    chrono::steady_clock::time_point tp_;
+
+    bool operator <(const scheduled_type<T> other) const
+    {
+      return this->data < other.data;
+    }
+  }; //end struct
+
+  template<typename T>
+  class sync_timed_queue : private sync_priority_queue<scheduled_type<T> >
+  {
+  public:
+    typedef sync_priority_queue<scheduled_type<T> > parent;
+
+    sync_timed_queue() : parent() {};
+	~sync_timed_queue() {} //Call parent?
+    BOOST_THREAD_NO_COPYABLE(sync_timed_queue)
+
+    using parent::size;
+    using parent::empty;
+
+    T pull();
+	bool try_pull(T& elem);
+
+    void push(const T& elem, chrono::steady_clock::time_point tp);
+    void push(const T& elem, chrono::steady_clock::duration dura);
+    bool try_push(const T& elem, chrono::steady_clock::time_point tp);
+    bool try_push(const T& elem, chrono::steady_clock::duration dura);
+
+  }; //end class
+
+} //end namespace
+
+#include <boost/config/abi_suffix.hpp>
+
+#endif
