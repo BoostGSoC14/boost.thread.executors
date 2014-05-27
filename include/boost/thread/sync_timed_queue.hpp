b3c4722b6142c0358c58acc9604079661ed1e839
Ian Forbes <ian.forbes@mail.mcgill.ca>
2014-05-26 20:49:33 -0400

    Added try_xyz functions. Added clock typedef which may move to a defaulted template parameter.

--- include/boost/thread/sync_timed_queue.hpp
+++ include/boost/thread/sync_timed_queue.hpp
@@ -28,6 +28,7 @@ namespace boost
   class sync_timed_queue : private sync_priority_queue<scheduled_type<T> >
   {
   public:
+    typedef chrono::steady_clock clock;
     typedef sync_priority_queue<scheduled_type<T> > parent;
 
     sync_timed_queue() : parent() {};
@@ -40,24 +41,82 @@ namespace boost
     T pull();
 	bool try_pull(T& elem);
 
-    void push(const T& elem, chrono::steady_clock::time_point tp);
-    void push(const T& elem, chrono::steady_clock::duration dura);
-    bool try_push(const T& elem, chrono::steady_clock::time_point tp);
-    bool try_push(const T& elem, chrono::steady_clock::duration dura);
+    void push(const T& elem, clock::time_point tp);
+    void push(const T& elem, clock::duration dura);
+    bool try_push(const T& elem, clock::time_point tp);
+    bool try_push(const T& elem, clock::duration dura);
 
   }; //end class
 
   template<typename T>
-  void sync_timed_queue<T>::push(const T& elem, chrono::steady_clock::time_point tp)
+  void sync_timed_queue<T>::push(const T& elem, clock::time_point tp)
   {
     parent::push(scheduled_type<T>(elem,tp));
   }
 
   template<typename T>
-  void sync_timed_queue<T>::push(const T& elem, chrono::steady_clock::duration dura)
+  void sync_timed_queue<T>::push(const T& elem, clock::duration dura)
   {
-    const chrono::steady_clock::time_point tp = chrono::steady_clock::now() + dura;
+    const chrono::steady_clock::time_point tp = clock::now() + dura;
     parent::push(scheduled_type<T>(elem,tp));
+  }
+
+  template<typename T>
+  bool sync_timed_queue<T>::try_push(const T& elem, clock::time_point tp)
+  {
+    return parent::try_push(scheduled_type<T>(elem,tp));
+  }
+
+  template<typename T>
+  bool sync_timed_queue<T>::try_push(const T& elem, clock::duration dura)
+  {
+    const clock::time_point tp = clock::now() + dura;
+    return parent::try_push(scheduled_type<T>(elem,tp));
+  }
+
+  template<typename T>
+  T sync_timed_queue<T>::pull()
+  {
+    unique_lock<mutex> lk(this->q_mutex_);
+    while(this->pq.empty() || (!this->pq.empty() && this->pq.top().tp > clock::now()) )
+    {
+        if(this->pq.empty)
+        {
+            this->empty.wait(lk);
+        }
+        else
+        {
+            this->empty.wait_until(lk,this->pq.top().tp);
+        }
+    }
+    const T temp = this->pq.top();
+    this->pq.pop();
+    return temp;
+  }
+
+
+  template<typename T>
+  bool sync_timed_queue<T>::try_pull(T& dest)
+  {
+    unique_lock<mutex> lk(this->q_mutex_);
+    if(lk.owns_lock())
+    {
+        while(this->pq.empty() || (!this->pq.empty() && this->pq.top().tp > clock::now()) )
+        {
+            if(this->pq.empty)
+            {
+                this->empty.wait(lk);
+            }
+            else
+            {
+                this->empty.wait_until(lk,this->pq.top().tp);
+            }
+        }
+        dest = this->pq.top();
+        this->pq.pop();
+        return true;
+    }
+    return false;
   }
 
 } //end namespace
