f37ec929c21f7f1a7b0e4b3f19bfab73e1951753
Ian Forbes <ian.forbes@mail.mcgill.ca>
2014-07-14 23:09:44 -0400

    Renamed files. Added a few more tests.

--- test/sync/mutual_exclusion/sync_pq/test_pq_single.cpp
+++ test/sync/mutual_exclusion/sync_pq/test_pq_single.cpp
@@ -13,9 +13,9 @@ void test_pull_for()
   boost::sync_priority_queue<int> pq;
   steady_clock::time_point start = steady_clock::now();
   boost::optional<int> val = pq.pull_for(milliseconds(500));
-  BOOST_TEST(!val);
   steady_clock::duration diff = steady_clock::now() - start;
-  BOOST_TEST(diff < milliseconds(510) && diff > milliseconds(490));
+  BOOST_TEST(!val);
+  BOOST_TEST(diff < milliseconds(510) && diff > milliseconds(500));
 }
 
 void test_pull_until()
@@ -23,9 +23,9 @@ void test_pull_until()
   boost::sync_priority_queue<int> pq;
   steady_clock::time_point start = steady_clock::now();
   boost::optional<int> val = pq.pull_until(start + milliseconds(500));
-  BOOST_TEST(!val);
   steady_clock::duration diff = steady_clock::now() - start;
-  BOOST_TEST(diff < milliseconds(510) && diff > milliseconds(490));
+  BOOST_TEST(!val);
+  BOOST_TEST(diff < milliseconds(510) && diff > milliseconds(500));
 }
 
 void test_pull_no_wait()
@@ -33,8 +33,8 @@ void test_pull_no_wait()
   boost::sync_priority_queue<int> pq;
   steady_clock::time_point start = steady_clock::now();
   boost::optional<int> val = pq.pull_no_wait();
-  BOOST_TEST(!val);
   steady_clock::duration diff = steady_clock::now() - start;
+  BOOST_TEST(!val);
   BOOST_TEST(diff < milliseconds(5));
 }
 
@@ -44,8 +44,8 @@ void test_pull_for_when_not_empty()
   pq.push(1);
   steady_clock::time_point start = steady_clock::now();
   boost::optional<int> val = pq.pull_for(milliseconds(500));
-  BOOST_TEST(val);
   steady_clock::duration diff = steady_clock::now() - start;
+  BOOST_TEST(val);
   BOOST_TEST(diff < milliseconds(5));
 }
 
@@ -55,8 +55,8 @@ void test_pull_until_when_not_empty()
   pq.push(1);
   steady_clock::time_point start = steady_clock::now();
   boost::optional<int> val = pq.pull_until(start + milliseconds(500));
-  BOOST_TEST(val);
   steady_clock::duration diff = steady_clock::now() - start;
+  BOOST_TEST(val);
   BOOST_TEST(diff < milliseconds(5));
 }
 
@@ -67,15 +67,30 @@ int main()
   BOOST_TEST(!pq.is_closed());
   BOOST_TEST_EQ(pq.size(), 0);
 
-  for(int i = 1; i <= 10; i++){
+  for(int i = 1; i <= 5; i++){
     pq.push(i);
     BOOST_TEST(!pq.empty());
     BOOST_TEST_EQ(pq.size(), i);
   }
-  for(int i = 10; i > 0; i--){
+
+  for(int i = 6; i <= 10; i++){
+    bool succ = pq.try_push(i);
+    BOOST_TEST(succ);
+    BOOST_TEST(!pq.empty());
+    BOOST_TEST_EQ(pq.size(), i);
+  }
+
+  for(int i = 10; i > 5; i--){
     int val = pq.pull();
     BOOST_TEST_EQ(val, i);
   }
+
+  for(int i = 5; i > 0; i--){
+    boost::optional<int> val = pq.try_pull();
+    BOOST_TEST(val);
+    BOOST_TEST_EQ(*val, i);
+  }
+
   BOOST_TEST(pq.empty());
   pq.close();
   BOOST_TEST(pq.is_closed());
