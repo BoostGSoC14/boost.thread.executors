da60da55c16e3c94fffee46f6e83102e3dcd58fe
Ian Forbes <ian.forbes@mail.mcgill.ca>
2014-07-19 22:50:57 -0400

    Now using boost::async and boost::future instead of std.
    Add test for pulling, pushing, and pulling and pushing in
    parallel.
    
    Getting bizarre errors when compiling with c++11.

--- test/sync/mutual_exclusion/sync_pq/test_pq_multi.cpp
+++ test/sync/mutual_exclusion/sync_pq/test_pq_multi.cpp
@@ -1,13 +1,46 @@
+#define BOOST_THREAD_VERSION 4
+
+#include <boost/thread/future.hpp>
+#include <boost/thread/barrier.hpp>
 #include <boost/chrono.hpp>
 #include <boost/thread/sync_priority_queue.hpp>
 
 #include <boost/detail/lightweight_test.hpp>
 
 using namespace boost::chrono;
-
-#include <future>
-    
+ 
 typedef boost::sync_priority_queue<int> sync_pq;
+
+struct call_push
+{
+  sync_pq& _pq;
+  boost::barrier& _go;
+  int _num;
+
+  call_push(sync_pq &q, boost::barrier& go, int n) : _pq(q), _go(go), _num(n) {}
+
+  typedef void result_type;
+  void operator()()
+  {
+    _go.count_down_and_wait();
+    _pq.push(_num);
+  }
+};
+
+struct call_pull
+{
+  sync_pq& _pq;
+  boost::barrier& _go;
+
+  call_pull(sync_pq& q, boost::barrier& go) : _pq(q), _go(go) {}
+
+  typedef int result_type;
+  int operator()()
+  {
+    _go.count_down_and_wait();
+    return _pq.pull();
+  }
+};
 
 void test_pull()
 {
@@ -21,10 +54,11 @@ void test_pull()
     BOOST_TEST(!pq.empty());
     BOOST_TEST_EQ(pq.size(),n);
 
-    std::future<int> futs[n];
+    boost::barrier b(n);
+    boost::future<int> futs[n];
     for(int i = 0; i < n; i++)
     {
-        futs[i] = std::async(std::launch::async,&sync_pq::pull,&pq);
+        futs[i] = boost::async(boost::launch::async, call_pull(pq,b));
     }
     for(int i = 0; i < n; i++)
     {
@@ -38,18 +72,48 @@ void test_push()
     const int n = 4;
     sync_pq pq;
     BOOST_TEST(pq.empty());
+
+    boost::barrier b(n);
+    boost::future<void> futs[n];
     for(int i  = 0; i < n; i++)
     {
-        // std::async(std::launch::async,[&](){pq.push(i);});
-        //Won't Compile with rvalue refs because overload resolution fails
-        std::async(std::launch::async,&sync_pq::push,&pq,i);
+        futs[i] = boost::async(boost::launch::async, call_push(pq,b,i));
+    }
+    for(int i = 0; i < n; i++)
+    {
+        futs[i].wait();
     }
     BOOST_TEST(!pq.empty());
     BOOST_TEST_EQ(pq.size(),n);
 }
 
+void test_both()
+{
+    const int n = 4;
+    sync_pq pq;
+    BOOST_TEST(pq.empty());
+
+    boost::barrier b(2*n);
+    boost::future<int> futs1[n];
+    boost::future<void> futs2[n];
+    for(int i  = 0; i < n; i++)
+    {
+        futs1[i] = boost::async(boost::launch::async, call_pull(pq,b));
+        futs2[i] = boost::async(boost::launch::async, call_push(pq,b,i));
+    }
+    for(int i = 0; i < n; i++)
+    {
+        futs1[i].wait();
+        futs2[i].wait();
+    }
+    BOOST_TEST(pq.empty());
+    BOOST_TEST_EQ(pq.size(), 0);
+}
+
 int main()
 {
     test_pull();
+    test_push();
+    test_both();
     return boost::report_errors();
 }
